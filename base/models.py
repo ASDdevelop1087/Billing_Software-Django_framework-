e04233729e7327e5228984ff4565dc57878bfc21
ASDdevelop1087 <asd.develop1087@gmail.com>
2026-08-11 23:17:13 +0500

    Remove UUIDField primary keys from models
    Removed UUIDField primary keys from Categorie and Product models.

--- base/models.py
+++ base/models.py
@@ -7,7 +7,6 @@ from django.db import models
 class Categorie(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True, blank=True)
-    id = models.UUIDField(primary_key=True, editable=False)
 
 
 class Product(models.Model):
@@ -15,5 +14,4 @@ class Product(models.Model):
     category = models.ForeignKey(
         Categorie, on_delete=models.CASCADE, null=True, blank=True, related_name='products')
     description = models.TextField(null=True, blank=True)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     price = models.FloatField()
